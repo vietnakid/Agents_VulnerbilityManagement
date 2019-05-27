beedf06d1807fd462aec516d103802d16e6792e9
root <root@localhost.localdomain>
2019-05-27 11:10:39 -0400

    namp fix

--- AgentNmap.py
+++ AgentNmap.py
@@ -51,13 +51,16 @@ def Main():
 
     host = '127.0.0.1'
 
-    port = 12345
+    port = 1234
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    s.connect((host, port))
-
-    data = s.recv(1024).decode('utf-8')      
+    s.bind((host, port))
+    s.listen()
+    c,addr = s.accept()
+    print('Connected to :', addr[0], ':', addr[1])
+    
+    data = c.recv(1024).decode('utf-8')      
     print(data)
 
     jdata= json.loads(data)
@@ -66,7 +69,7 @@ def Main():
     result =r.scan_options(jdata)
     print(result)
 
-    s.send(result.encode('utf-8'))
+    c.send(result.encode('utf-8'))
 
     s.close()
 
