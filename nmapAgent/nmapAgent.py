10ebf58b535187ec5fa9b8daefd6c3ad5110176b
JokerLee04601 <manhldse04601@gamil.com>
2019-06-02 10:53:27 +0700

    Refresh modul nmap

--- nmapAgent/nmapAgent.py
+++ nmapAgent/nmapAgent.py
@@ -4,59 +4,46 @@ import socket
 import json
 import pickle
 import nmap
+import os
+import time
+import xmltodict
+
 
 class Nmap():
-    def nmap_scanFull(self,ip):
-        nm =nmap.PortScanner()
-        jdata = json.dumps(nm.scan(ip),indent=4)
-        print(type(jdata))
-        return jdata
+    def __init__(self):
+        self.fileName = ""
 
-    def nmap_detectOS(self,ip):
-        nm =nmap.PortScanner()
-        jdata = json.dumps(nm.scan(ip, arguments='-O' ),indent=4)
-        return jdata
+    def gen_cmd(self, target):
+        opt = ['nmap', '-oX', self.fileName, '-A', target]
+        cmd = ' '.join(opt)
+        return cmd
 
-    def nmap_PortRangeScan(self,ip):
-        nm =nmap.PortScanner()
-        jdata = json.dumps(nm.scan(ip, arguments="-sn -n -T5"),indent=4)
-        return jdata
+    def gen_fileName(self):
+        times = time.ctime()
+        self.fileName = time.strftime('%Y%m%d-%H%M%S.txt')
 
-class Run():
-    def format_Actions(self,request):
-        actions_String= request['actions']
-        list_actions= actions_String.split(',')
-        for i,item in enumerate (list_actions):
-            list_actions[i] =list_actions[i].strip()
-        return list_actions
-    def scan_options(self,request):
-         nm= Nmap()
-         result = {
-             "Status" :"OK"
-         }
-         ip = request["ip"]
-         actions =self.format_Actions(request)
-         if (len(actions)>0):
-             for ac in actions:
-                if(ac == "scan"):
-                     result_scan= json.loads(nm.nmap_scanFull(ip))
-                     result.update(result_scan)
-                if (ac == "os"):
-                     result_OS = json.loads(nm.nmap_detectOS(ip))
-                     result.update(result_OS)
-                if (ac == "port"):
-                     result_port = json.loads(nm.nmap_PortRangeScan(ip))
-                     result.update(result_port)
-         return json.dumps(result,indent=4)
+    def runCmds(self, target):
+        cmd = self.gen_cmd(self.fileName)
+        os.system(cmd)
+        print('Done')
+
+    def parse_XMLtoJson(self):
+        f = open(self.fileName)
+        xml_content = f.read()
+        f.close()
+        result = json.dumps(xmltodict.parse(
+            xml_content, xml_attribs=True), indent=4, sort_keys=True)
+        return result
+
 
 def main():
-    rawData = input()
-    jData = json.loads(rawData)
-    r = Run()
-    result =r.scan_options(jData)
+    target = input()
+    nm = Nmap()
+    nm.gen_fileName()
+    nm.runCmds(target)
+    result = nm.parse_XMLtoJson()
     print(result)
-    
+
 
 if __name__ == "__main__":
     main()
-
