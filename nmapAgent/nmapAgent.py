4664e7b7d07a550c149bfad80cb3ce915784ebd8
root <ductase04515@fpt.edu.vn>
2019-06-08 08:44:16 +0000

    Change nmap to -T5 mode scan Save log to /tmp/lognmap

--- nmapAgent/nmapAgent.py
+++ nmapAgent/nmapAgent.py
@@ -12,13 +12,13 @@ class Nmap():
         self.fileName = ""
 
     def gen_cmd(self, target):
-        opt = ['nmap', '-oX', self.fileName, '-A', target, '> /dev/null 2>&1']
+        opt = ['nmap', '-oX', self.fileName, '-A', target, '-T5', '> /dev/null 2>&1']
         cmd = ' '.join(opt)
         return cmd
 
     def gen_fileName(self, target):
         times = time.ctime()
-        self.fileName = time.strftime(target+'_%Y%m%d-%H%M%S.xml')
+        self.fileName = time.strftime('/tmp/lognmap/' + target + '_%Y%m%d-%H%M%S.xml')
 
     def runCmds(self, target):
         cmd = self.gen_cmd(target)
