87f45064a78fe49560f7490f2cc143bd2a67169a
AnhDuc <anhduc@AnhDucs-MacBook-Pro.local>
2019-06-16 12:09:41 +0700

    9.18_DeployWappalyzerAgent Using Popen to speed up scan

--- wappalyzerAgent/wappalyzerAgent.py
+++ wappalyzerAgent/wappalyzerAgent.py
@@ -2,33 +2,30 @@ import subprocess
 import json
 import os
 
-def run_cmd(targetObject):
-	prefix = ['http://', 'https://']
-	for pre in prefix:
-		finalTarget = pre + targetObject.get('target') + ':' + targetObject.get('port') + '/'
-		cmd = ['wappalyzer', finalTarget]
-		FNULL = open(os.devnull, 'w')
-		result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=FNULL)
-		_result = result.stdout.decode('utf-8')
-		__result = json.loads(_result)
-		if 'error' not in __result.get('urls').get(finalTarget):
-			return __result
-	return None
-
 def main():
 	targetJson = input()
 	targetObject = json.loads(targetJson)
 	openports = targetObject.get('openports')
+	openports.append('')
 	resultObject = dict()
 	resultObject['result'] = dict()
 	resultObject['target'] = targetObject.get('target')
-	for i in openports:
-		targetObject['port'] = str(i)
-		_resultObject = run_cmd(targetObject)
-		if _resultObject != None:
-			resultObject['result'][i] = _resultObject
-
-	print (json.dumps(resultObject))
+	jobs = []
+	prefix = ['http://', 'https://']
+	for openport in openports:
+		for pre in prefix:
+			finalTarget = pre + targetObject.get('target') + ':' + openport + '/'
+			cmd = ['wappalyzer', finalTarget]
+			result = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+			jobs.append(result)
+	for job in jobs:
+		output = job.communicate()[0]
+		_result = output.decode('utf-8')
+		__result = json.loads(_result)
+		finalTarget = list(__result.get('urls').keys())[0]
+		if 'error' not in __result.get('urls').get(finalTarget):
+			resultObject['result'][finalTarget] = __result
+	print (json.dumps(resultObject.))
 
 if __name__ == "__main__":
 	main()
