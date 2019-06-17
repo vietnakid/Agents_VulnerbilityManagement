cbf36b3968755654dd2aa233ea5d8af63d87158e
KiD <vietnase62219@fpt.edu.vn>
2019-06-17 14:30:21 +0700

    10.02_CVESearchAgents - Code CVESearch Agent to communicate through socket - Writing deploying script - Writing readme.MD for deploy

--- README.md
+++ README.md
@@ -18,12 +18,19 @@ mkdir -p /var/log/nse
 ### Deploy Agent
 
 * For Nmap Agent:
-	Change directory to `nmapAgent` and run `python3 deploy.py`
+	* Install nmap, xinetd
+	* Change directory to `nmapAgent` and run `python3 deploy.py`
 
 * For NSE Agent:
-	Change directory to `nseAgent` and run `python3 deploy.py`
+	* Install nmap, xinetd
+	* Change directory to `nseAgent` and run `python3 deploy.py`
 
 * For Wappalyzer Agent:
 	* Install nodejs and npm first (Google scan for this step)
 	* Use npm to install wappalyzer package in `https://www.npmjs.com/package/wappalyzer` by this command `npm i -g wappalyzer`
 	* Change directory to `wappalyzerAgent` and run `python3 deploy.py`
+
+* For CVE-Search Agent:
+	* Install xinetd
+	* If you not using docker, you have to re-configure in the deploy.py file. We recommend to use docker at here: https://hub.docker.com/r/ttimasdf/cve-search/
+	* Change directory to `cveSearchAgent` and run `python3 deploy.py`
---
+++ cveSearchAgent/cveSearchAgent.py
@@ -0,0 +1,43 @@
+import json
+import subprocess
+import traceback
+
+def main():
+    jData = input()
+    data = json.loads(jData)
+
+    cpes = data.get('cpes', [])
+    jobs = []
+
+    for cpe in cpes:
+        cmd = ['python3', '/opt/cve/bin/search.py', '-p', cpe, '-o', 'json']
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        jobs.append((cpe, process))
+
+    outputResult = dict()
+    
+    for job in jobs:
+        try:
+            process = job[1]
+            jOutputs = process.communicate()[0].decode('utf-8').split('\n')
+            cpeOutput = []
+            for jOutput in jOutputs:
+                try:
+                    output = json.loads(jOutput)
+                    res = dict()
+                    res['cve'] = output.get("id")
+                    res['cvss'] = output.get("cvss")
+                    res['cwe'] = output.get("cwe")
+                    cpeOutput.append(res)
+                except Exception as e:
+                    pass
+            cpe = job[0]
+            outputResult[cpe] = cpeOutput
+        except Exception as e:
+            print("Exception =", e)
+            traceback.print_exc()
+    print(json.dumps(outputResult))
+
+if __name__ == "__main__":
+    # {"cpes": ["python:3.6.5", "excel"]}
+    main()
